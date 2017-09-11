9ec82db882aa9432c7203d31a51d81af8eab55d4
Timothy Root <timothy@email.arizona.edu>
2017-09-10 20:58:42 -0700

    <3

--- Hello.java
+++ Hello.java
@@ -9,7 +9,8 @@ public class Hello {
     }
 
     public static void callSign() {
-        
+
+	System.out.println("Lock S-foils in attack position.");        
         // Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
@@ -26,7 +27,7 @@ public class Hello {
  
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
-
+	System.out.println("I copy, gold leader!");
     }
 
 }
